e8fa7d08952f7a0391fedd258e58b6627031f3ac
Hakimov Bulat <bulathakimov1704@gmail.com>
2024-12-11 18:10:58 +0300

    Добавьте файлы проекта.

---
+++ Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба_10
+{
+    internal class Manager
+    {
+        public static Frame MainFrame {get; set;}
+    }
+}
